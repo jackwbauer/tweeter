0e54d8ef5ac25312bfd59f09bbf2dc7585b57c3e
Jack Bauer <bauer.jackw@gmail.com>
2018-11-06 02:37:43 +0000

    counter turns red if < 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $('form textarea').on('keyup', function() {
         const remainingChars = 140 - $('form textarea')[0].textLength;
-        $('.counter').html(remainingChars);
+        const $counter = $('.counter');
+        $counter.html(remainingChars);
+        if(remainingChars <= 0) {
+            $counter.addClass('invalid');
+        } else {
+            $counter.removeClass('invalid');
+        }
     })
 });
