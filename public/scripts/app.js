fee4c71b303d50ce3d04df39f05ceb61a71d19e5
Jack Bauer <bauer.jackw@gmail.com>
2018-11-06 22:38:54 +0000

    dynamically rendering tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,87 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+  
+  function renderTweets(tweets) {
+      const $tweetContainer = $('.tweet-container');
+      tweets.forEach((tweet) => {
+        const tweetElement = createTweetElement(tweet);
+        $tweetContainer.append(tweetElement);
+      });
+    // loops through tweets
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+  }
+  
+  function createTweetElement(tweet) {
+    const $tweet = $('<article>', { class: 'tweet' });  
+    const $header = $('<header>');
+    const $avatar = $('<img>', { class: 'userIcon' }).attr('src', tweet.user.avatars.small);
+    const $username = $('<span>', { class: 'username'}).text(tweet.user.name);
+    const $handle = $('<span>', { class: 'handle' }).text(tweet.user.handle);
+    $header.append($avatar, $username, $handle);
+
+    const $tweetDiv = $('<div>');
+    const $tweetText = $('<p>').text(tweet.content.text);
+    $tweetDiv.append($tweetText);
+
+    const $footer = $('<footer>');
+    const $timestamp = $('<span>').text(`${Math.floor((new Date()-tweet.created_at) / 86400000)} days`);
+    const $heart = $('<i>', { class: 'fas fa-heart fa-lg'});
+    const $retweet = $('<i>', { class: 'fas fa-retweet fa-lg'});
+    const $flag = $('<i>', { class: 'fas fa-flag fa-lg'});
+    $footer.append($timestamp, $heart, $retweet, $flag);
+
+    $tweet.append($header, $tweetDiv, $footer);
+    return $tweet;
+  }
+  $(document).ready(() => {
+    renderTweets(data);
+  })
+  
